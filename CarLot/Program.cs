aa635930dce703904a1c51bee3518c4659ca06e3
lindsayw89 <lindsay.89@outlook.com>
2019-07-26 23:17:38 -0500

    changes 726 1117

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -10,17 +10,24 @@ namespace CarLot
     {
         static void Main(string[] args)
         {
-            CarLot Lot1 = new CarLot();
-            CarLot Lot2 = new CarLot();
-            Vehicle car1 = new Car("GHY548","Honda", "Civic", 24000.50, "hatchback", 4);
-            Vehicle car2 = new Car("GHY548", "kia", "Civic", 19000.80, "coupe", 4);
+            CarLot LotA = new CarLot();
+            CarLot LotB = new CarLot();
+            Vehicle car1 = new Car("GHY546","Honda", "Civic", 24000.50, "hatchback", 4);
+            Vehicle car2 = new Car("KHV848", "kia", "Civic", 19000.80, "coupe", 4);
+            Vehicle truck1 = new Truck("ghys564", "ford", "f150", 40000.5, "short");
+            Vehicle truck2 = new Truck("ghys564", "GMC", "Canyon", 50000.5, "Standard");
+            //car1.GiveDescription();
 
-            car1.GiveDescription();
+            LotA.AddVehicle(car1);
+            LotA.AddVehicle(car2);
+            LotA.AddVehicle(truck1);
+            LotA.AddVehicle(truck2);
 
-            Lot1.AddVehicle(car1);
 
-            Vehicle truck1 = new Truck("ghys564", "ford", "f150", 40000.5, "8 feet");
-            truck1.GiveDescription();
+            //truck1.GiveDescription();
+
+            LotA.PrintInventory();
+            
             Console.ReadKey();
         }
     }
@@ -28,16 +35,24 @@ namespace CarLot
     {
         string Name;
         static List<Vehicle> ListOfVehicles =new List<Vehicle>();
-
+      
         public  void AddVehicle(Vehicle automobile)
         {
 
             ListOfVehicles.Add(automobile);
         }
-        public  void PrintInventory()
+        public void PrintInventory()
         {
-            //foreach (Vehicle v in ListOfVehicles)
-               //Console.WriteLine(v.GiveDescription());
+            foreach (Vehicle v in ListOfVehicles)
+                //Console.WriteLine(v.GiveDescription());
+                v.GiveDescription();
+                
+                       
+            
+           // for( i=0; i<3; i++)
+           // {
+          //      Console.WriteLine(i.GiveDescription());
+          //  }
         }
     }
 }
