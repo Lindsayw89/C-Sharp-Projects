129ded65ad99abb859a90a8c414a4f9d77941b1e
lindsayw89 <lindsay.89@outlook.com>
2019-07-28 09:53:35 -0500

    changes 728 953

--- CarLot/Program.cs
+++ CarLot/Program.cs
@@ -10,8 +10,8 @@ namespace CarLot
     {
         static void Main(string[] args)
         {
-            CarLot LotA = new CarLot();
-            CarLot LotB = new CarLot();
+            CarLot LotA = new CarLot("Smith Ford Lot");
+            CarLot LotB = new CarLot("Annie Oakley Lot");
             Vehicle car1 = new Car("GHY546","Honda", "Civic", 24000.50, "hatchback", 4);
             Vehicle car2 = new Car("KHV848", "kia", "Civic", 19000.80, "coupe", 4);
             Vehicle truck1 = new Truck("ghys564", "ford", "f150", 40000.5, "short");
@@ -21,21 +21,29 @@ namespace CarLot
             LotA.AddVehicle(car1);
             LotA.AddVehicle(car2);
             LotA.AddVehicle(truck1);
-            LotA.AddVehicle(truck2);
+            LotB.AddVehicle(truck2);
 
 
             //truck1.GiveDescription();
 
-            LotA.PrintInventory();
-            
+           LotA.PrintInventory();
+            LotB.PrintInventory();
+
             Console.ReadKey();
         }
     }
     public class CarLot
     {
-        string Name;
-        static List<Vehicle> ListOfVehicles =new List<Vehicle>();
-      
+        public string Name;
+        List<Vehicle> ListOfVehicles;
+
+
+
+        public CarLot(string name)
+        {
+            Name = name;
+          ListOfVehicles = new List<Vehicle>();
+        }
         public  void AddVehicle(Vehicle automobile)
         {
 
@@ -43,6 +51,7 @@ namespace CarLot
         }
         public void PrintInventory()
         {
+            Console.WriteLine(Name);
             foreach (Vehicle v in ListOfVehicles)
                 //Console.WriteLine(v.GiveDescription());
                 v.GiveDescription();
